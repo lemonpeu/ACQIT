289373be1ed344e72b8e3887dfab4059b2e80eff
Peu <illanesrpaula@gmail.com>
2021-10-29 03:23:45 -0300

    Adding sending emails

--- components/common/boxusername/boxusername.js
+++ components/common/boxusername/boxusername.js
@@ -1,44 +1,45 @@
-import styles from './boxusername.module.scss';
-import Input from '../input/input';
-import Image from '../Image/Image';
+import styles from "./boxusername.module.scss";
+import Input from "../input/input";
+import Image from "../Image/Image";
+import ContactForm from "../../sections/ContactForm/ContactForm";
 
 const BoxUserName = (props) => {
-    const { icon, name, img, alt, text, boxUserName } = props;
+  const { icon, name, img, alt, text, boxUserName } = props;
 
-    return (
-        <section
-            className={`${boxUserName ? styles.boxUserNameStyle : styles.boxIconText} ${
-                styles.containerBox
-            }`}
-        >
-            <div className={styles.container}>
-                {icon && (
-                    <Image
-                        width={110}
-                        height={110}
-                        layout="fixed"
-                        className={styles.iconContainer}
-                        src={img}
-                        alt={alt}
-                    />
-                )}
-                {boxUserName ? (
-                    <div className={styles.welcomeContainer} id="contact">
-                        <p className={styles.text}>Hola {name}</p>
-                        <div className={styles.desktopVersion}>
-                            <p className={styles.text}>¿Necesitas asistencia?</p>
-                            <label htmlFor="email" className={styles.text}>
-                                Dejanos tu e-mail
-                            </label>
-                            <Input placeholder="E-mail." id="email" />
-                        </div>
-                    </div>
-                ) : (
-                    <p className={styles.text}>{text}</p>
-                )}
+  return (
+    <section
+      className={`${
+        boxUserName ? styles.boxUserNameStyle : styles.boxIconText
+      } ${styles.containerBox}`}
+    >
+      <div className={styles.container}>
+        {icon && (
+          <Image
+            width={110}
+            height={110}
+            layout="fixed"
+            className={styles.iconContainer}
+            src={img}
+            alt={alt}
+          />
+        )}
+        {boxUserName ? (
+          <div className={styles.welcomeContainer} id="contact">
+            <p className={styles.text}>Hola {name}</p>
+            <div className={styles.desktopVersion}>
+              <p className={styles.text}>¿Necesitas asistencia?</p>
+              <label htmlFor="email" className={styles.text}>
+                Dejanos tu e-mail
+              </label>
+              <ContactForm />
             </div>
-        </section>
-    );
+          </div>
+        ) : (
+          <p className={styles.text}>{text}</p>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default BoxUserName;
